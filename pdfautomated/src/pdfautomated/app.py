a1812dbb59be39a2a74ab3cc85bf9d2a3f5faf0d
chenkianwee <chenkianwee@gmail.com>
2025-08-10 21:11:17 +0900

    successfully build for ubuntu and windows

--- pdfautomated/src/pdfautomated/app.py
+++ pdfautomated/src/pdfautomated/app.py
@@ -86,7 +86,7 @@ class PdfAutomated(toga.App):
         main_box = toga.Box(style=Pack(align_items='center'))
         main_box.add(tabs_container)
         # main window
-        self.main_window = toga.MainWindow(title=self.formal_name, size=(900, 800))
+        self.main_window = toga.MainWindow(title=self.formal_name, size=(700, 600))
         self.main_window.content = main_box
         self.main_window.show()
     
